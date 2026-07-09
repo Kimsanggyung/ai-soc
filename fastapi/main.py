effacd8859bdbbe78645e7a6ac53762ca14e0cdb
Kimsanggyung <ksang0906@gmail.com>
2026-07-09 10:06:12 +0900

    set react

--- fastapi/main.py
+++ fastapi/main.py
@@ -24,4 +24,5 @@ def root_check():
     return {"status": "healthy", "platform": "AI-SOC SOAR Core Backend"}
 
 if __name__ == "__main__":
+    
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
